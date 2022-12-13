8a4df830edfe693bbd5f0a4ac7cae27cc9326a3d
Peter Brottveit Bock <post@peterbb.net>
2022-12-13 20:30:24 +0100

    remove uneccesary call to asSequence

--- src/main/kotlin/day01.kt
+++ src/main/kotlin/day01.kt
@@ -2,13 +2,13 @@ fun day1part1(lines: Lines): Int =
     lines
         .batchByNewline()
         .maxOfOrNull { batch ->
-            batch.asSequence().sumOf { it.toInt() }
+            batch.sumOf { it.toInt() }
         } !!
 
 fun day1part2(lines: Lines): Int =
     lines
         .batchByNewline()
-        .map { batch -> batch.asSequence().sumOf { it.toInt() } }
+        .map { batch -> batch.sumOf { it.toInt() } }
         .sortedDescending()
         .take(3)
         .sum()
